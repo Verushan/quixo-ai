5cb2a24ac0468785991e312b274a3de7d12951bf
Verushan <verushannaidoo@gmail.com>
2023-09-05 17:29:17 +0200

    main.py will do Human vs Minimax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from quixo import MinimaxAgent, RandomAgent, GameManager, HumanAgent
 
 manager = GameManager()
-agent1 = RandomAgent()
+agent1 = HumanAgent()
 agent2 = MinimaxAgent()
 manager.play_match(agent1, agent2)
